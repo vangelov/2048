5aeab402a22584d5c8647cada919e4cd1cef7f2e
Vladi <vlady@Vladis-MacBook-Pro.local>
2018-05-13 18:06:25 +0300

    Refactor moveBoard

--- src/state/moves/moveBoard.js
+++ src/state/moves/moveBoard.js
@@ -1,5 +1,75 @@
 import * as actions from './actions';
 
+const turnsToNormalizeForActionType = {
+  [actions.MOVE_LEFT]: 0,
+  [actions.MOVE_UP]: 1,
+  [actions.MOVE_RIGHT]: 2,
+  [actions.MOVE_DOWN]: 3
+};
+
+const turnsTypesCount = Object.keys(turnsToNormalizeForActionType).length;
+
+/* We have the logic the move the board to the left so we transform all
+  other cases to this */
+export default function moveBoard(board, actionType) {
+  const turns = turnsToNormalizeForActionType[actionType];
+  let rotatedBoard = board;
+
+  // 'Normalize' the board
+  for (let i = 0; i < turns; i++) {
+    rotatedBoard = rotateLeft(rotatedBoard);
+  }
+
+  let { movedBoard: movedAndRotatedBoard, scoreWon } = moveLeft(rotatedBoard);
+
+  // Bring it back to the original orientation
+  for (let i = 0; i < turnsTypesCount - turns; i++) {
+    movedAndRotatedBoard = rotateLeft(movedAndRotatedBoard);
+  }
+
+  return { movedBoard: movedAndRotatedBoard, scoreWon };
+}
+
+function rotateLeft(board) {
+  const rotatedBoard = [];
+  const rowsCount = board.length;
+  const columnsCount = board[0].length;
+
+  for (let i = 0; i < rowsCount; i++) {
+    rotatedBoard.push([]);
+
+    for (let j = 0; j < columnsCount; j++) {
+      rotatedBoard[i][j] = board[j][columnsCount - i - 1];
+    }
+  }
+
+  return rotatedBoard;
+};
+
+/* For each row we move left, merge and (if necessary ) move left again */
+function moveLeft(board) {
+  const movedBoard = [];
+  let scoreWon = 0;
+
+  for (const row of board) {
+    let newRow = moveRowLeft(row);
+    const {
+      mergedRow,
+      rowScoreWon,
+      madeMerge
+    } = mergeRowFromLeft(newRow);
+
+    if (madeMerge) {
+      newRow = moveRowLeft(mergedRow);;
+    }
+
+    scoreWon += rowScoreWon;
+    movedBoard.push(newRow);
+  }
+
+  return { movedBoard, scoreWon };
+}
+
 function moveRowLeft(row) {
   const newRow = Array(row.length).fill(0);
   let k = 0;
@@ -31,69 +101,4 @@ function mergeRowFromLeft(row) {
     rowScoreWon,
     madeMerge: mergedJ !== null
   };
-}
-
-function moveLeft(board) {
-  const movedBoard = [];
-  let scoreWon = 0;
-
-  for (const row of board) {
-    let newRow = moveRowLeft(row);
-    const {
-      mergedRow,
-      rowScoreWon,
-      madeMerge
-    } = mergeRowFromLeft(newRow);
-
-    if (madeMerge) {
-      newRow = moveRowLeft(mergedRow);;
-    }
-
-    scoreWon += rowScoreWon;
-    movedBoard.push(newRow);
-  }
-
-  return { movedBoard, scoreWon };
-}
-
-function rotateLeft(board) {
-  const rotatedBoard = [];
-  const rowsCount = board.length;
-  const columnsCount = board[0].length;
-
-  for (let i = 0; i < rowsCount; i++) {
-    rotatedBoard.push([]);
-
-    for (let j = 0; j < columnsCount; j++) {
-      rotatedBoard[i][j] = board[j][columnsCount - i - 1];
-    }
-  }
-
-  return rotatedBoard;
-};
-
-const turnsToNormalizeForActionType = {
-  [actions.MOVE_LEFT]: 0,
-  [actions.MOVE_UP]: 1,
-  [actions.MOVE_RIGHT]: 2,
-  [actions.MOVE_DOWN]: 3
-};
-
-const turnsTypesCount = Object.keys(turnsToNormalizeForActionType).length;
-
-export default function moveBoard(board, actionType) {
-  const turns = turnsToNormalizeForActionType[actionType];
-  let rotatedBoard = board;
-
-  for (let i = 0; i < turns; i++) {
-    rotatedBoard = rotateLeft(rotatedBoard);
-  }
-
-  let { movedBoard: movedAndRotatedBoard, scoreWon } = moveLeft(rotatedBoard);
-
-  for (let i = 0; i < turnsTypesCount - turns; i++) {
-    movedAndRotatedBoard = rotateLeft(movedAndRotatedBoard);
-  }
-
-  return { movedBoard: movedAndRotatedBoard, scoreWon };
 }
